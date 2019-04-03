43811be0b44003e2f9983ee63da2e55a23edaae8
Emanuel Azage <eazage@usc.edu>
2019-04-02 19:22:09 -0700

    class demo

---
+++ __tests__/api/playlist-test.js
@@ -0,0 +1,15 @@
+const frisby = require('frisby');
+
+const { Joi } = frisby;
+
+// it('should return 204 when deleting a playlist that exists', () => {
+//   return frisby
+//           .del('http://localhost:8000/api/playlists/6')
+//           .expect('status', 204);
+// });
+//
+// it('should return 404 when deleting a playlist that does not exist', () => {
+//   return frisby
+//           .del('http://localhost:8000/api/playlists/-1')
+//           .expect('status', 404);
+// });
